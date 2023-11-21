4008fcd35ef0acb8d7ace0952a8c68a718cb1a9a
Sihao Xiao <sixiao@ucsd.edu>
2023-11-21 15:28:29 +0000

    added philospher model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_112104) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_150012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,23 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_112104) do
     t.datetime "date", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "philosophers", force: :cascade do |t|
+    t.integer "age"
+    t.string "location"
+    t.string "availability"
+    t.text "bio"
+    t.integer "price_per_hour"
+    t.string "preferred_topic"
+    t.string "philosophical_focus"
+    t.string "dress_code"
+    t.string "language"
+    t.string "communication_style"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_philosophers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +53,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_112104) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "philosophers", "users"
 end
